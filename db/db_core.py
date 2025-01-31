972e7e4bef7c444989c11d1956bfb5c088bee1e1
Nicki tst <kanieleoutiz@gmail.com>
2025-01-31 19:26:02 +0300

    add comments

--- db/db_core.py
+++ db/db_core.py
@@ -4,8 +4,10 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sess
 from db.config import db_config
 from db.models import Base
 
+# Создаем движок БД
 engine = create_async_engine(db_config.url(), echo=True)
 
+# Создаем фабрику сессий
 AsyncSessionLocal = async_sessionmaker(
     bind=engine,
     class_=AsyncSession,
@@ -13,10 +15,12 @@ AsyncSessionLocal = async_sessionmaker(
     autocommit=False,
 )
 
+# Управление сессиями
 async def get_session() -> AsyncSession:
     async with AsyncSessionLocal() as session:
         yield session
 
+# Создаем таблицы в БД
 async def create_tables():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
