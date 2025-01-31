79cc656687973c1954a036d4574539167a5cfe07
Nicki tst <kanieleoutiz@gmail.com>
2025-01-31 20:29:24 +0300

    add server

---
+++ server/server_main.py
@@ -0,0 +1,29 @@
+import os
+
+from dotenv import load_dotenv
+from fastapi import FastAPI, Request
+
+from bot.bot_main import Bot
+
+# Загружаем переменные из .env
+load_dotenv()
+token = os.getenv("TOKEN")
+
+# Создаем бота
+bot = Bot(token)
+
+# Инициализируем FastAPI
+app = FastAPI()
+
+@app.get("/")
+async def home():
+    return {"message": "Bot is running!"}
+
+@app.post("/")
+async def recieve_update(request: Request):
+    """Process input Webhook-requests from Telegram"""
+    update = await request.json()
+    await bot.process_update(update)
+    return {"ok": True}
+
+
