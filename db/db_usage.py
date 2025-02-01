95eb727192000b5debcec36311a2fb1a29af2b75
Nicki tst <kanieleoutiz@gmail.com>
2025-02-01 18:07:50 +0300

    change generate_short_url()

--- db/db_usage.py
+++ db/db_usage.py
@@ -39,11 +39,24 @@ class DatabaseManager:
             print("❌ Введённый текст не является ссылкой, короткая не создаётся.")
             return None
 
+        # Генерируем код и проверяем его уникальность
         short_code = generate_short_code()
+        result = await self.session.execute(
+            select(OutputUrl)
+            .where(OutputUrl.short_url == short_code)
+        )
+        # Если такой код уже есть – генерируем новый, пока не получим уникальный
+        while result.scalar_one_or_none() is not None:
+            short_code = generate_short_code()
+            result = await self.session.execute(
+                select(OutputUrl)
+                .where(OutputUrl.short_url == short_code)
+            )
+
         short_entry = OutputUrl(
             user_id=input_entry.user_id,
             input_url_id=input_entry.id,
-            short_url=short_code
+            short_url=short_code # сохраняем только код, без домена
         )
 
         self.session.add(short_entry)
