524a3c8192fa1f8681d3ddebcdef71e148f59c70
Nicki tst <kanieleoutiz@gmail.com>
2025-02-01 17:09:49 +0300

    add redirect_short_url()

--- server/server_main.py
+++ server/server_main.py
@@ -1,10 +1,12 @@
 import os
 
 from dotenv import load_dotenv
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
+from starlette.responses import RedirectResponse
 
 from bot.bot_main import Bot
-from db.db_core import create_tables
+from db.db_core import create_tables, AsyncSessionLocal
+from db.db_usage import DatabaseManager
 
 # Загружаем переменные из .env
 load_dotenv()
@@ -34,4 +36,13 @@ async def recieve_update(request: Request):
     await bot.process_update(update)
     return {"ok": True}
 
-
+@app.get("/{short_code}")
+async def redirect_short_url(short_code: str):
+    # Функция для поиска оригинальной ссылки по коду в БД
+    async with AsyncSessionLocal() as session:
+        db_manager = DatabaseManager(session)
+        original_url = await db_manager.get_original_url(short_code)
+    if original_url:
+        return RedirectResponse(url=original_url)
+    else:
+        raise HTTPException(status_code=404, detail="Ссылка не найдена")
