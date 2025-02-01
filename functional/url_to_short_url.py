8c2d3c60e5d87167c2443752c19b4f5fd31a0f9f
Nicki tst <kanieleoutiz@gmail.com>
2025-02-01 17:50:14 +0300

    change method: now we get only len=8 code

--- functional/url_to_short_url.py
+++ functional/url_to_short_url.py
@@ -2,10 +2,5 @@ import string
 import random
 
 
-def url_to_short_url() -> str:
-    main_url = "https://url-to-short-url.onrender.com/"
-    code = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
-    short_url = main_url + code
-
-    return short_url
-
+def generate_short_code() -> str:
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=8))
